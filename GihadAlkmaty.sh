fa0e4cb95fba460edc8cb4fa198a41cef2de89a2
jihadLkmaty218 <ahgjihadmeords19771@gmail.com>
2017-04-21 00:18:27 +0200

    Add files via upload

---
+++ GihadAlkmaty.sh
@@ -0,0 +1,141 @@
+#!/bin/bash
+
+clear
+#DEFINED COLOR SETTINGS
+RED=$(tput setaf 1 && tput bold)
+GREEN=$(tput setaf 2 && tput bold)
+STAND=$(tput sgr0)
+BLUE=$(tput setaf 6 && tput bold)
+
+
+
+echo ""
+echo ""
+echo ""
+echo $RED"              ████████████████████████████████████████████████"
+echo $RED"              █                                              █"
+echo $RED"              █     Gihad Alkmaty from Libya                 █"
+echo $RED"              █                                              █"
+echo $RED"              █            tool  Version 2.0 Download        █"
+echo $RED"              █                                              █"
+echo $RED"              █                                              █"
+echo $RED"              ████████████████████████████████████████████████"
+echo ""
+echo $BLUE"     ﺏﻮﻴﺗﻮﻳ ﻰﻠﻋ ﻲﺗﺎﻨﻗ https://www.youtube.com/channel/UCubdeDNFXMJDOyM9Xa6xRfg"
+echo ""
+echo $BLUE"     ﺮﺜﻳﻮﺗ ﻰﻠﻋ ﻲﺑﺎﺴﺣ  https://twitter.com/GihadAlkmaty"
+sleep 3
+clear
+
+
+
+echo ""
+echo $RED"                   ██████████████████████████████████████"
+echo $RED"                   ██   1   Download MS16-032           █"
+echo $RED"                   ██   2   Download CVE-2017-3881 cisco█"
+echo $RED"                   ██   3.  Download MS16-098           █"
+echo $RED"                   ██   4  Download passwords windows10 █"
+echo $RED"                   ██   5.  Download MS17-0100          █"
+echo $RED"                   ██   6.  Download TpmInitUACBypass   █"
+echo $RED"                   ██   7.  ﺎﻧﺍ ﻲﻠﻋ ﺕﺎﻣﻮﻠﻌﻣ             █"
+echo $RED"                   ██   8.  EXIT                        █"
+echo $RED"                   ██████████████████████████████████████"
+
+echo $BLUE"       ﺕﺍﺮﻐﺘﻟ​ﺍ ﻞﻳﺰﻨﺘﻟ ﻲﻃﺎﻤﻘﻟ​ﺍ ﺯﺎﻬﺟ ﺓﺍﺩﺍ ﻲﻓ ﻚﺑ ﺎﺒﺣﺮﻣ"$STAND
+read menuoption
+
+if [ $menuoption = "1" ]; then
+echo "ﺮﺘﻧﺍ ﻂﻐﺿﺍ"
+read target
+firefox https://github.com/jihadLkmaty218/MS16-032.ps1
+echo ""
+read -p "ﺮﺘﻧﺍ ﻂﻐﺿﺍ"
+./GihadAlkmaty.sh
+else
+
+
+if [ $menuoption = "2" ]; then
+echo "ﺮﺘﻧﺍ ﻂﻐﺿﺍ"
+read target
+dig $target any
+echo ""
+echo ""
+firefox https://github.com/jihadLkmaty218/CVE-2017-3881-exploit-cisco-
+echo ""
+read -p "ﺮﺘﻧﺍ ﻂﻐﺿﺍ"
+./GihadAlkmaty.sh
+else
+
+if [ $menuoption = "3" ]; then
+echo "ﺮﺘﻧﺍ ﻂﻐﺿﺍ"
+read target
+echo ""
+echo $STAND"ﺮﺘﻧﺍ ﻂﻐﺿﺍ "
+read IFACE;
+echo ""
+echo ""
+firefox https://github.com/jihadLkmaty218/MS16-098
+./GihadAlkmaty.sh
+else
+
+if [ $menuoption = "4" ]; then
+echo "ﺮﺘﻧﺍ ﻂﻐﺿﺍ"
+read target
+echo ""
+cd /usr/bin
+firefox https://github.com/jihadLkmaty218/script-Which-has-been-developed-Fetch-passwords-windows-7-8-10-
+echo ""
+read -p "ﺮﺘﻧﺍ ﻂﻐﺿﺍ"
+./GihadAlkmaty.sh
+else
+
+if [ $menuoption = "5" ]; then
+echo "ﺮﺘﻧﺍ ﻂﻐﺿﺍ"
+read target
+echo ""
+cd /usr/bin
+firefox https://github.com/jihadLkmaty218/MS17-0100
+echo ""
+read -p "ﺮﺘﻧﺍ ﻂﻐﺿﺍ"
+./GihadAlkmaty.sh
+else
+
+if [ $menuoption = "6" ]; then
+echo "ﺮﺘﻧﺍ ﻂﻐﺿﺍ"
+read target
+echo ""
+cd /root
+firefox https://github.com/jihadLkmaty218/TpmInitUACBypass
+echo ""
+read -p "ﺮﺘﻧﺍ ﻂﻐﺿﺍ"
+./GihadAlkmaty.sh
+else
+
+echo "" ﺺﺼﺨﺘﻣ ﻲﺗﺎﻣﻮﻠﻌﻣ ﻦﻣﺍ ﻑﺮﺘﺤﻣ ﻲﻃﺎﻤﻘﻟ​ﺍ ﺩﺎﻬﺟ
+echo ""php html css java script ajax mysql sql server CMS Angular JS 2 + Firebase mongodb python shell script ruby CMS  CISCO C++  
+echo "" ﺲﻟﺪﻧﻻ​ﺍ ﻲﺣ ﺲﻠﺑﺮﻃ ﻲﻓ ﻦﻜﺴﻧ ﺔﻨﺴﻟ​ﺍ 17 ﺮﻤﻌﻟ​ﺍ ﻦﻣ ﻎﻟ​ﺎﺑ
+if [ $menuoption = "8" ]; then
+exit
+fi
+fi
+fi
+fi
+fi
+fi
+fi
+fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
